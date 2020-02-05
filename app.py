dda2987f2cc8756cf2c471a33000a04e1b410fca
tomoyk <tmyk.kym@gmail.com>
2020-02-05 09:56:53 +0900

    add app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify, render_template
+import os
+import json
+
+from j import esxi
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False  # JSONでの日本語文字化け対策
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('main.tpl', machines=esxi().values())
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3000)
