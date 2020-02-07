bb96a157902dda0e7b0f715c2aa390120fd769b2
tomoyk <tmyk.kym@gmail.com>
2020-02-07 21:19:05 +0900

    refactoring

--- connect.py
+++ connect.py
@@ -8,24 +8,15 @@ client.load_system_host_keys()
 
 machines = []
 
-""" VM一覧に追加 """
-def _add_vm_info(dat, esxi_hostname):
-    stdin, stdout, stderr = client.exec_command('vim-cmd vmsvc/power.getstate {}'.format(dat[0]))
-    result = stdout.readlines()
-    # client.close()
+""" VMのリストを取得 """
+def get_vms_list():
+    # VM情報一覧の2行目～を取得(ラベルを除外)
+    stdin, stdout, stderr = client.exec_command('vim-cmd vmsvc/getallvms')
+    vm_info = [line.strip('\n').split() for line in stdout][1:]
 
-    if 'Suspended\n' in result:
-        power =  'suspend'
-    elif 'Powered on\n' in result:
-        power = 'on'
-    elif 'Powered off\n' in result:
-        power =  'off'
-    else:
-        power = 'unknown'
-
-    return ({
-        'uniq_id': esxi_hostname+'|'+dat[0],
-        'physical_host': esxi_hostname,
+    return [{
+        'uniq_id':dat[0],
+        # 'physical_host': esxi_hostname,
         'id': dat[0],
         'name': dat[1],
         'datastore': dat[2],
@@ -33,19 +24,34 @@ def _add_vm_info(dat, esxi_hostname):
         'guest_os': dat[4],
         'vm_version': dat[5],
         'comment': ' '.join(dat[6:]),
-        'power': power
-    })
+    } for dat in vm_info]
     
 
-def _get_vm_list(esxi_hostname):
-    stdin, stdout, stderr = client.exec_command('vim-cmd vmsvc/getallvms')
-    # VM情報一覧の2行目～を取得(ラベルを除外)
-    vm_info = [line.strip('\n').split() for line in stdout][1:]
-    # 非同期実行
-    cors = [_add_vm_info(vm, esxi_hostname) for vm in vm_info]
-    # results = await asyncio.gather(*cors)
-    # await loop.run_in_executor(None, time.sleep, sec)
-    return cors
+""" VMの電源状態の一覧を取得 """
+def get_vms_power():
+    # VMの電源一覧を取得
+    stdin, stdout, stderr = client.exec_command("""
+    for id in `vim-cmd vmsvc/getallvms | sed '/^Vmid.*$/d' | awk '{print $1}'`
+    do
+      echo -n $id/
+      vim-cmd vmsvc/power.getstate $id | grep -v Retrieved
+    done
+    """)
+
+    # VMの電源一覧を整形
+    result = {}
+    for line in stdout.readlines():
+        vmid, state = line.split('/')
+        if 'Suspended' in state:
+            result[vmid] = 'suspend'
+        elif 'Powered on' in state:
+            result[vmid] = 'on'
+        elif 'Powered off' in state:
+            result[vmid] = 'off'
+        else:
+            result[vmid] = 'unknown'
+
+    return result
 
 
 def _get_esxi_hosts():
@@ -69,44 +75,6 @@ def _get_esxi_hosts():
 #     stdin, stdout, stderr = client.exec_command(f'vim-cmd vmsvc/power.{state} {vmid}')
 #     client.close()
 #     return stdout
-# 
-# 
-# async def _get_vm_power(my_vmid):
-#     esxi_hostname, vmid = my_vmid.split('|')
-#     # get esxi hosts
-#     esxi_hosts = _get_esxi_hosts()
-#     # connect target esxi host
-#     host = esxi_hosts.get(esxi_hostname)
-#     client.connect(
-#         hostname=host.get('hostname'),
-#         username=host.get('username'),
-#         password=host.get('password')
-#     )
-#     stdin, stdout, stderr = client.exec_command(f'vim-cmd vmsvc/power.getstate {vmid}')
-#     loop = asyncio.get_event_loop()
-#     import time
-#     # await loop.run_in_executor(None, time.sleep)
-#     await loop.run_in_executor(None, client.exec_command, f'vim-cmd vmsvc/power.getstate {vmid}')
-# 
-#     result = stdout.readlines()
-#     client.close()
-# 
-#     if 'Suspended\n' in result:
-#         return 'suspend'
-#     elif 'Powered on\n' in result:
-#         return 'on'
-#     elif 'Powered off\n' in result:
-#         return 'off'
-#     else:
-#         return 'unknown'
-
-
-# def _get_vm_power_async(my_vmid_lst):
-#     loop = asyncio.get_event_loop()
-#     gather = asyncio.gather(
-#         *[_get_vm_power(vmid) for vmid in my_vmid_lst]
-#     )
-#     loop.run_until_complete(gather)
 
 
 def init_vm():
@@ -119,23 +87,8 @@ def init_vm():
             password=param.get('password')
         )
         # VM一覧を取得
-        # loop = asyncio.get_event_loop()
-        # vmlst = _get_vm_list(param.get('hostname'))
-        hostname = param.get('hostname')
-        # results = loop.run_until_complete(
-        vm_formated_info.append(_get_vm_list(hostname))
-        # vmdct_format = _bind_uniq_vmid(vmlst, host)
-        # print(results)
-        # a = _get_vm_power_async(vmdct_format.keys())
-        # print(a)
-
-        # update power status info
-        # for my_vmid,vm_info in vmdct_format.items():
-        #     vm_info['power'] = _get_vm_power(my_vmid)
-        #     # my_vmid を key にしたdictへ _get_vm_power() から代入する？
-        
-        # vm_formated_info.update(vmdct_format)
-        # client.close()
+        result = get_vms_list()
+        vm_formated_info.append(result)
     
     return vm_formated_info
 
@@ -143,8 +96,8 @@ def init_vm():
 def main():
     result = init_vm()
     for esx in result:
-        for vm in esx:
-            print(vm['power'])
+        for p in esx:
+            print(p)
 
 
 if __name__ == '__main__':
