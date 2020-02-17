752ee6b8543cf2c497715baec9a899ee55f7baa0
tomoyk <tmyk.kym@gmail.com>
2020-02-17 16:13:30 +0900

    add endpoint 'add-vm'

--- app.py
+++ app.py
@@ -24,10 +24,37 @@ def detail(uniq_id):
 def create_vm():
     return render_template('create.html', title='CREATE VM')
 
+
+@app.route('/v1/add', methods=['POST'])
+def add_vm():
+    """
+    curl -s -XPOST -d '{"name":"myvm","cpu":1,"ram":500,"ssd":30,"network":"dmz","os":"ubuntu1804","user":"myadmin","password":"my12pass34word56","hostname":"myvm","tag":"foo,bar","memo":"this is memo"}'  "http://192.168.100.3:3000/v1/add"
+    """
+    # Parse Request
+    req_data = request.get_data()
+    req_txt = req_data.decode('utf-8')
+    # Get Request body
+    payload = json.loads(req_txt)
+    vm_spec = {
+        "name": payload.get('name'),
+        "cpu": payload.get('cpu'),
+        "ram": payload.get('ram'),
+        "ssd": payload.get('ssd'),
+        "network": payload.get('network'),
+        "os": payload.get('os'),
+        "user": payload.get('user'),
+        "password": payload.get('password'),
+        "hostname": payload.get('hostname'),
+        "tag": payload.get('tag'),
+        "memo": payload.get('memo'),
+    }
+    return jsonify(payload)
+
+
 @app.route('/v1/power/<string:uniq_id>', methods=['POST'])
 def set_power(uniq_id):
     """
-    curl -s -XPOST -d '{"state": "on"}'  "http://192.168.100.3:3000/power/jasmine|38"
+    curl -s -XPOST -d '{"state": "on"}'  "http://192.168.100.3:3000/v1/power/jasmine|38"
     """
     # Parse Request
     req_data = request.get_data()
