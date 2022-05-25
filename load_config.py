3b224afd445bab1f98992c4b6f09aa05b94c95cc
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2022-05-25 11:12:46 +0900

    型アノテーションを追加

--- load_config.py
+++ load_config.py
@@ -14,8 +14,12 @@ class HostsConfig:
     installer_iso_path: pathlib.Path
 
 
-def get_esxi_nodes():
-    """ ESXi一覧をファイルから取得 """
+def get_esxi_nodes() -> dict[str, HostsConfig]:
+    """ ESXi一覧をファイルから取得
+    Return::
+        key: esxi node name
+        value: esxi node detail
+    """
 
     if os.environ.get('HOSTS_PATH'):
         HOSTS_PATH = str(os.environ.get('HOSTS_PATH'))
@@ -25,13 +29,16 @@ def get_esxi_nodes():
     with open(HOSTS_PATH) as f:
         hosts_config = yaml.safe_load(f.read())
 
+    result: dict[str, HostsConfig] = {}
     for esxi_nodename, conf in hosts_config.items():
         print("Validating:", esxi_nodename)
         try:
-            HostsConfig(**conf)
+            result[esxi_nodename] = HostsConfig(**conf)
         except Exception as e:
             print("Fail to validate:", HOSTS_PATH)
             raise e
+
+    return result
 
 
 if __name__ == "__main__":
