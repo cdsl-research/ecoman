4e65f7c5df190d2ea43e93846b19b9493685e242
tomoyk <tmyk.kym@gmail.com>
2022-05-26 12:45:26 +0000

    MongoDBにデータをストアする実装を追加

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,8 +1,12 @@
 import pathlib
 from dataclasses import asdict, dataclass
 from ipaddress import IPv4Address
+from typing import List
+import os
 
 import paramiko
+from pymongo import MongoClient, UpdateOne
+from pymongo.errors import ConnectionFailure, OperationFailure
 
 import connecter
 import load_config
@@ -24,14 +28,14 @@ class MachineSpecCrawled:
     esxi_node_address: str
 
 
-def main():
+def crawl() -> List[MachineSpecCrawled]:
     """ Init ssh connecter """
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.load_system_host_keys()
 
     """ Load Config """
-    machines_info = []
+    machines_info: List[MachineSpecCrawled] = []
     nodes_conf = load_config.get_esxi_nodes()
     for esxi_nodename, config in nodes_conf.items():
         # try:
@@ -64,7 +68,43 @@ def main():
             spec = MachineSpecCrawled(**vm_info)
             machines_info.append(spec)
         
-        # print(machines_info)
+    # print(machines_info)
+    return machines_info
+
+
+def register(machines_info: List[MachineSpecCrawled]):
+    MONGO_USERNAME = os.getenv("MONGO_USERNAME", "root")
+    MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "example")
+    MONGO_DBNAME = os.getenv("MONGO_DBNAME", "machines")
+    MONGO_HOST = os.getenv("MONGO_HOST", "127.0.0.1")
+    # MONGO_CONNECTION_STRING = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}"
+    MONGO_CONNECTION_STRING = f"mongodb://{MONGO_HOST}/"
+
+    client = MongoClient(MONGO_CONNECTION_STRING)
+    client.admin.command('ping')
+    db = client[MONGO_DBNAME]
+    # collection = db["machines"]
+    from mongo_ipv4_codec import codec_options
+    collection = db.get_collection("machines", codec_options=codec_options)
+
+    filter_ = {}
+    records = [asdict(msc) for msc in machines_info]
+    bulk_replaces = [
+        UpdateOne({
+            'id': rec['id'], 
+            'esxi_node_name': rec['esxi_node_name']
+        }, {
+            '$set': rec
+        }, upsert=True) for rec in records
+    ]
+    # print(records)
+    collection.bulk_write(bulk_replaces)
+
+
+
+def main():
+    c = crawl()
+    register(machines_info=c)
 
 
 if __name__ == "__main__":
