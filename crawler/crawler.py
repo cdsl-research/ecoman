874c15cf3266ad03098239c0491f9c58462a18e1
tomoyk <tmyk.kym@gmail.com>
2022-05-27 00:23:06 +0000

    ログと例外処理を追加

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -55,23 +55,30 @@ class MachineSpecCrawled:
 def get_vms_list(_client: paramiko.SSHClient) -> Dict[int, MachineDetail]:
     """ VMのリストを取得 """
 
+    print("Start get_vms_list")
     # VM情報一覧の2行目～を取得(ラベルを除外)
-    _, stdout, _ = _client.exec_command('vim-cmd vmsvc/getallvms')
+    _, stdout, stderr = _client.exec_command('vim-cmd vmsvc/getallvms')
+    print("stderr:", stderr)
+
     vm_info: Dict[int, MachineDetail] = {}
     for line in stdout.readlines():
         # 数字から始まる行
         if re.match(r'^\d+', line):
-            dat = line.strip('\n').split()
-            vmid = int(dat[0])
-            vm_info[vmid] = MachineDetail(
-                id=vmid,
-                name=dat[1],
-                datastore=dat[2],
-                datastore_path=dat[3],
-                guest_os=dat[4],
-                vm_version=dat[5],
-                comment=' '.join(dat[6:])
-            )
+            try:
+                dat = line.strip('\n').split()
+                vmid = int(dat[0])
+                vm_info[vmid] = MachineDetail(
+                    id=vmid,
+                    name=dat[1],
+                    datastore=dat[2],
+                    datastore_path=dat[3],
+                    guest_os=dat[4],
+                    vm_version=dat[5],
+                    comment=' '.join(dat[6:])
+                )
+            except Exception as e:
+                print("Fail to create MachineDetail: dat=", dat)
+                continue
 
         # Vmidから始まる行
         elif line.startswith("Vmid"):
@@ -83,19 +90,26 @@ def get_vms_list(_client: paramiko.SSHClient) -> Dict[int, MachineDetail]:
 def get_vms_power(_client: paramiko.SSHClient) -> Dict[int, PowerStatus]:
     """ VMの電源状態のリストを取得 """
 
+    print("Start get_vms_power")
     # VMの電源一覧を取得
-    _, stdout, _ = _client.exec_command(r"""
+    _, stdout, stderr = _client.exec_command(r"""
     for id in `vim-cmd vmsvc/getallvms | grep '^[0-9]\+' | awk '{print $1}'`
     do
       vim-cmd vmsvc/power.getstate $id | grep -v Retrieved | sed "s/^/$id|/g" &
     done
     """)
+    print("stderr:", stderr)
 
     # VMの電源一覧を整形
     result: Dict[int, PowerStatus] = {}
     for line in stdout.readlines():
-        _vmid, state = line.split('|')
-        vmid = int(_vmid)
+        try:
+            _vmid, state = line.split('|')
+            vmid = int(_vmid)
+        except Exception as e:
+            print("Exception:", e)
+            continue
+
         if 'Suspended' in state:
             result[vmid] = PowerStatus.SUSPEND
         elif 'Powered on' in state:
@@ -103,6 +117,7 @@ def get_vms_power(_client: paramiko.SSHClient) -> Dict[int, PowerStatus]:
         elif 'Powered off' in state:
             result[vmid] = PowerStatus.OFF
         else:
+            print("Power unknown: vmid =", vmid)
             result[vmid] = PowerStatus.UNKNOWN
 
     return result
@@ -111,22 +126,30 @@ def get_vms_power(_client: paramiko.SSHClient) -> Dict[int, PowerStatus]:
 def get_vms_ip(_client: paramiko.SSHClient) -> Dict[int, IPv4Address]:
     """ VMのIPアドレスのリストを取得 """
 
-    _, stdout, _ = _client.exec_command(r"""
+    print("Start get_vms_ip")
+    _, stdout, stderr = _client.exec_command(r"""
     for id in `vim-cmd vmsvc/getallvms | grep '^[0-9]\+' | awk '{print $1}'`
     do
       vim-cmd vmsvc/get.summary $id | grep ipAddress | grep -o \"[0-9a-f:\.]\\+\" | sed "s/\"//g;s/^/$id|/g" &
     done
     """)
+    print("stderr:", stderr)
 
     result: Dict[int, IPv4Address] = {}
     for line in stdout.readlines():
-        vmid, ipaddr = line.split('|')
-        result[int(vmid)] = IPv4Address(ipaddr.strip())
+        try:
+            vmid, ipaddr = line.split('|')
+            result[int(vmid)] = IPv4Address(ipaddr.strip())
+        except Exception as e:
+            print("Exception", e)
+            continue
 
     return result
 
 
 def crawl() -> List[MachineSpecCrawled]:
+    print("Start crawling")
+
     """ Init ssh connecter """
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -136,16 +159,17 @@ def crawl() -> List[MachineSpecCrawled]:
     machines_info: List[MachineSpecCrawled] = []
     nodes_conf = load_config.get_esxi_nodes()
     for esxi_nodename, config in nodes_conf.items():
-        # try:
-        # VMにSSH接続
-        client.connect(
-            config.addr,
-            username=config.username,
-            key_filename=config.identity_file_path,
-            timeout=5.0
-        )
-        # except paramiko.ssh_exception.SSHException as e:
-        #     print(e)
+        print("Connect to", esxi_nodename)
+        try:
+            client.connect(
+                config.addr,
+                username=config.username,
+                key_filename=config.identity_file_path,
+                timeout=5.0
+            )
+        except paramiko.ssh_exception.SSHException as e:
+            print(e)
+            continue
 
         # VM一覧を結合
         vm_list: dict[int, MachineDetail] = get_vms_list(
@@ -174,7 +198,7 @@ def crawl() -> List[MachineSpecCrawled]:
 def register(machines_info: List[MachineSpecCrawled]):
     MONGO_USERNAME = os.getenv("MONGO_USERNAME", "root")
     MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "example")
-    MONGO_DBNAME = os.getenv("MONGO_DBNAME", "machines")
+    MONGO_DBNAME = os.getenv("MONGO_DBNAME", "ecoman")
     MONGO_HOST = os.getenv("MONGO_HOST", "127.0.0.1")
     # MONGO_CONNECTION_STRING = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}"
     MONGO_CONNECTION_STRING = f"mongodb://{MONGO_HOST}/"
